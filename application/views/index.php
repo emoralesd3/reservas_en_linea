98b2c4508a5ee126e20f781dda3f173a4bca18d2
Elvin Morales <elvinmorales48@gmail.com>
2019-11-08 07:48:45 -0600

    proyecto inicial de reservas en linea

---
+++ application/views/index.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags --> 
+
+    <!-- Title -->
+    <title>posada de la piedad</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+
+    <!-- Core Stylesheet -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Responsive CSS -->
+    <link href="assets/css/responsive/responsive.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+
+</head>
+
+<body>
+
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header_area" id="header">
+        <div class="container h-100">
+            <div class="row h-100">
+                <div class="col-12 h-100">
+                    <nav class="h-100 navbar navbar-expand-lg align-items-center">
+                        <a class="navbar-brand" href="index.html">posada de la piedad </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#caviarNav" aria-controls="caviarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="fa fa-bars"></span></button>
+                        <div class="collapse navbar-collapse" id="caviarNav">
+                            <ul class="navbar-nav ml-auto" id="caviarMenu">
+
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#home">Inicio <span class="sr-only">(current)</span></a>
+                                </li>
+
+
+
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#about">nosotros</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#menu">eventos</a>
+                                </li>
+
+                                   <li class="nav-item">
+                                    <a class="nav-link" href="cartilla.html">menu</a>
+                                </li>
+
+
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#reservation">Reservaciones</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.html">Contacto</a>
+                                </li>
+                                
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="login.html">loginja</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="reserva.html">reserva de habitaciones</a>
+                                </li>
+                                
+                                
+                            </ul>
+
+
+                               
+                                    <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="login1.1/index.php">¿&oacute; login1.1?</a></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+                
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ****** Welcome Area Start ****** -->
+    <section class="caviar-hero-area" id="home">
+        <!-- Welcome Social Info -->
+        <div class="welcome-social-info">
+            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        </div>
+        <!-- Welcome Slides -->
+        <div class="caviar-hero-slides owl-carousel">
+
+
+            <!-- Single Slidessobre nosotros   img/bg-img/hero-1.jpg             -->
+            <div class="single-hero-slides bg-img" style="background-image: url(img/bg-img/1.jpg);"> 
+                <div class="container h-100">
+                    <div class="row h-100 align-items-center">
+                        <div class="col-11 col-md-6 col-lg-4">
+                            <div class="hero-content">
+                                <h2></h2>
+                                <p>TODO AVENTURA INICIA CON UN "SI"
+Ubicado en la periferia de la cabecera municipal de San Felipe, Retalhuleu. A 614 metros sobre el nivel del mar, lo que hace un clima cálido; con su abundante vegetación lo vuelve agradable y característico de la boca costa.</p>
+                                <!--<a href="#" class="btn caviar-btn"><span></span> Reservacion</a>-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+
+                <!-- prueba de segunda imagen cambiante-->
+                <div class="hero-slides-nav bg-img" style="background-image: url(img/bg-img/2.jpg);"></div>
+            </div>
+            
+
+
+            <!-- Single Slides -->
+            <div class="single-hero-slides bg-img" style="background-image: url(img/bg-img/2.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-center">
+                        <div class="col-11 col-md-6 col-lg-4">
+                            <div class="hero-content">
+                                <h2></h2>
+                                <p>Posada de la Piedad se encuentra ubicada a tan solo 5 minutos de los Parques Recreativos Xetulul y Xocomil. Es el lugar ideal para descansar, relajarse y pasar un tiempo agradable con su familia y amigos.</p>
+                                <!-- BOTON DE RESERVA<a href="#" class="btn caviar-btn"><span></span> Reservation</a-->
+                            </div>
+                        </div>
+                    </div>
+                </div> <!--acá termina la prueba de la segunda imagen cambiate-->
+
+
+                <!-- Slider Nav -->
+                <div class="hero-slides-nav bg-img" style="background-image: url(img/bg-img/1.jpg);"></div>
+            </div>
+        </div>
+    </section>
+    <!-- ****** Welcome Area End ****** -->
+
+    <!-- ****** About Us Area Start ****** -->
+    <section class="caviar-about-us-area section-padding-150" id="about">
+        <div class="container">
+            <!-- About Us Single Area -->
+            <div class="row align-items-center">
+                <div class="col-12 col-md-6">
+                    <div class="about-us-thumbnail wow fadeInUp" data-wow-delay="0.5s">
+                        <img src="img/menu-img/6.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-5 ml-md-auto">
+                    <div class="section-heading">
+                        <h2>nosotros</h2>
+                    </div>
+                    <div class="about-us-content">
+                        <span>posada de la piedad</span>
+                        <p>Nos esforzaremos para que usted cambie de ambiente y logre un descanso reparador que lo dejara con ganas de volvernos a visita.</p>
+                    </div>
+                </div>
+            </div>
+            
+
+        </div>
+    </section>
+    <!-- ****** About Us Area End ****** -->
+
+
+
+
+    <!-- ****** Dish Menu Area Start  IMAGENES DE PLATILLOS (HABITACIONES)****** -->
+    <section class="caviar-dish-menu" id="menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 menu-heading">
+                    <div class="section-heading text-center">
+                        <h2>eventos</h2>
+                    </div>
+                    <!-- btn -->
+                    <a href="eventos.html" class="btn caviar-btn"><span></span>ver eventos </a>
+                </div>
+
+
+            </div>
+            <div class="row">
+                <div class="col-12 col-sm-6 col-md-4">
+                    <div class="caviar-single-dish wow fadeInUp" data-wow-delay="0.5s">
+                        <img src="img/menu-img/1.jpg" alt="">
+                        <div class="dish-info">
+                            <h6 class="dish-name"></h6>
+                            <p class="dish-price"></p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-sm-6 col-md-4">
+                    <div class="caviar-single-dish wow fadeInUp" data-wow-delay="1s">
+                        <img src="img/menu-img/2.jpg" alt="">
+                        <div class="dish-info">
+                            <h6 class="dish-name"></h6>
+                            <p class="dish-price"></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4">
+                    <div class="caviar-single-dish wow fadeInUp" data-wow-delay="1.5s">
+                        <img src="img/menu-img/3.jpg" alt="">
+                        <div class="dish-info">
+                            <h6 class="dish-name"></h6>
+                            <p class="dish-price"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- ****** Reservation Area Start   RESERVACIONES  ****** -->
+    <section class="caviar-reservation-area d-md-flex align-items-center" id="reservation">
+        <div class="reservation-form-area d-flex justify-content-end">
+            <div class="reservation-form">
+                <div class="section-heading">
+                    <h2>Reservación</h2>
+                </div>
+                <form action="#">
+                    <div class="row">
+                        <div class="col-12 col-lg-6">
+                            <input type="date" class="form-control">
+                        </div>
+                        <div class="col-12 col-lg-6">
+                            <input type="time" class="form-control">
+                        </div>
+                        <div class="col-12 col-lg-6">
+                            <input type="text" class="form-control" placeholder="Nombre">
+                        </div>
+                        <div class="col-12 col-lg-6">
+                            <input type="text" class="form-control" placeholder="Apellido">
+                        </div>
+                        <div class="col-12">
+                            <textarea name="reservation-message" class="form-control" id="reservationMessage" cols="30" rows="10" placeholder="Mensaje"></textarea>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn caviar-btn"><span></span> Reserva de habitacion</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="reservation-side-thumb wow fadeInRightBig" data-wow-delay="0.5s">
+            <img src="img/menu-img/7.jpg" alt="">
+        </div>
+    </section>
+
+     <br><br><br><br>
+
+     <!--area del estado del clima -->
+     <div id="climainfo">
+         <div><div class="about-us-content">
+                        <span>en esta area podra ver el estado del clima</span>
+                        </div></div>
+     </div>
+
+
+     <div id="clima">
+        <div id="c_4aa7e182fbb10bdd07866ccb9900ae94" class="normal"></div><script type="text/javascript" src="https://www.clima.com/widget/widget_loader/4aa7e182fbb10bdd07866ccb9900ae94"></script>
+     </div>
+
+
+
+   <!-- weather widget start --><div id="m-booked-custom-widget-70599"> <div class="weather-customize" style="width:500px;"> <div class="booked-weather-custom-160 color-009f5d" style="width:500px;" id="width5"> <div class="booked-weather-custom-160-date">Tiempo, 07 Noviembre</div> <div class="booked-weather-custom-160-main more"> <a target="_blank" href="https://hotelmix.es/weather/retalhuleu-97629" class="booked-weather-custom-160-city"> Tiempo en Retalhuleu </a> <a target="_blank" class="booked-wzs-bottom-custom-160" href="https://www.booked.net/"><img src="//s.bookcdn.com/images/letter/s5.gif" alt="Booked" /></a> <div class="booked-weather-custom-160-degree booked-weather-custom-C wmd18"><span><span class="plus">+</span>24</span></div> <div class="booked-weather-custom-details"> <p><span>Alta: <strong><span class="plus">+</span>24<sup>°</sup></strong></span><span> Baja: <strong><span class="plus">+</span>22<sup>°</sup></strong></span></p> <p>Humedad: <strong>98%</strong></p> <p>Viento: <strong>WNW - 7 KPH</strong></p> </div> </div> </div></div> </div><script type="text/javascript"> var css_file=document.createElement("link"); css_file.setAttribute("rel","stylesheet"); css_file.setAttribute("type","text/css"); css_file.setAttribute("href",'https://s.bookcdn.com/css/weather.css?v=0.0.1'); document.getElementsByTagName("head")[0].appendChild(css_file); function setWidgetData(data) { if(typeof(data) != 'undefined' && data.results.length > 0) { for(var i = 0; i < data.results.length; ++i) { var objMainBlock = document.getElementById('m-booked-custom-widget-70599'); if(objMainBlock !== null) { var copyBlock = document.getElementById('m-bookew-weather-copy-'+data.results[i].widget_type); objMainBlock.innerHTML = data.results[i].html_code; if(copyBlock !== null) objMainBlock.appendChild(copyBlock); } } } else { alert('data=undefined||data.results is empty'); } } </script> <script type="text/javascript" charset="UTF-8" src="https://widgets.booked.net/weather/info?action=get_weather_info&ver=6&cityID=97629&type=2&scode=124&ltid=3457&domid=582&anc_id=12954&cmetric=1&wlangID=4&color=009f5d&wwidth=338&header_color=ffffff&text_color=333333&link_color=08488D&border_form=1&footer_color=ffffff&footer_text_color=333333&transparent=0"></script><!-- weather widget end -->
+
+
+
+      <!--fin del area del estado del clima -->
+ 
+
+
+    <!-- ****** fin el area de reservacion ****** -->
+
+    <!-- ****** Footer Area Start ****** -->
+    <footer class="caviar-footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="footer-text">
+                        <a href="#" class="navbar-brand">lapiedad</a>
+                        <p>posada de la piedad 2018</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<!--aca empieza el modal 
+-->
+<div id="cookieChoiceInfo" style="position: fixed; width: 320px; background-color: rgb(138, 134, 9); color: rgb(255, 255, 255); right: 6px; bottom: 6px; padding: 4px; z-index: 1000; text-align: left;"><span>en este mes descuento en mesas dobles al llenar el siguiente formulario </span><a href="static/legal/politica_cookies.php" target="_blank" style="margin-left: 0px; color: rgb(255, 255, 255);"> </a><a id="cookieChoiceDismiss" href="#" style="margin-left: 5px; color: rgb(255, 255, 255);">Aceptar</a>
+<a style="margin-left: 5px; color: rgb(255, 255, 255);" href="#" id="cookieChoiceDismiss">registrarse</a>
+<br><br>
+<input type="submit" value="reservar" onclick = "location.href='formulario de reserva.html'"llevame>
+</div><br><br></h>
+
+</div>
+<!--termina el modal>
+
+
+
+    </footer>
+    <!-- ****** Footer Area End ****** -->
+
+    <!-- Jquery-2.2.4 js -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap-4 js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/others/plugins.js"></script>
+    <!-- Active JS -->
+    <script src="js/active.js"></script>
+</body>
